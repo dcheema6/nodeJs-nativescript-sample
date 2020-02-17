4ee93808cc60884a835dc8b40f3b5b7f37dc54b0
dcheema6 <dcheema6@uwo.ca>
2020-02-17 12:22:38 -0500

    added search route and a default view

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,28 @@
+//==========  IMPORTS  ========================================
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
+//=========ROUTE IMPORTS==================================
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var searchRouter = require('./routes/search');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//========BODY PARSER CONFIGURATION==================================
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//========ROUTE CONFIG==================================
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/server.js
@@ -0,0 +1,14 @@
+var app = require('./app')
+var port = 3000;
+
+//middleware for developement only --be sure to delete before release
+app.use(function (request, response, next) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header('Access-Control-Allow-Methods', 'POST, PATCH, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
+//======== OPEN CONNECTION ============================
+app.listen(port);
+console.log('Listening on port ' + port + ' ');
